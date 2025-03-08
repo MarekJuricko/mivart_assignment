d376eb27bcd6d309a613a6064f38e9e2fcefb2ac
Marek Juričko <marek.juricko@azet.sk>
2025-03-08 16:12:54 +0100

    created components and styles folder, created HeroSection component

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/HeroSection.css"
+
+const HeroSection = () => {
+  return (
+    <div className='heroSection'>
+        <a href="" className='menu'> Menu
+            <img src="" alt=""/>
+        </a>
+        <div className='title'>
+            <h1>Lorem ipsum dolor</h1>
+        </div>
+        <div className='logo'></div>
+    </div>
+  )
+}
+
+export default HeroSection
